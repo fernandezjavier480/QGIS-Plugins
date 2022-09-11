28efe487ecf2e3a6cc3350dc49ac743aad84d6e3
JavierFernandez <fernandezjavier480@gmail.com>
2022-09-11 10:13:12 -0500

    Plugin creation start All files without modifications, was generated by QGIS

---
+++ test/Prueba.py
@@ -0,0 +1,5 @@
+from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
+from qgis.PyQt.QtGui import QIcon
+from qgis.PyQt.QtWidgets import QAction
+from qgis.core import QgsVectorLayer, QgsField
+import os.path
